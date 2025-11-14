04a736de99d79522c27785fb3256e8b506feecfe
LozanoD40 <lozano.mora.diego@gmail.com>
2025-11-14 17:09:03 -0500

    Add files via upload

---
+++ BACKEND/config/db.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      'mongodb+srv://jacobogarcesoquendo:aFJzVMGN3o7fA38A@cluster0.mqwbn.mongodb.net/diegoLozanoMora'
+    )
+    console.log('MongoDB connected')
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err)
+  }
+}
---
+++ BACKEND/models/Datauser.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose'
+
+const datauserSchema = new mongoose.Schema({
+  usuarioId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  juegoId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Game',
+  },
+  logrosDesbloqueados: [
+    { type: mongoose.Schema.Types.ObjectId, ref: 'Achievement' },
+  ],
+  interaccion: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }],
+  juegoSubido: [{ type: String, default: 'owner' }],
+  horasJugadas: { type: Number, default: 0 },
+  level: { type: Number, min: 0, max: 80, default: 0 },
+  genero: { type: String, default: '' },
+  loginCount: { type: Number, default: 0 },
+  tiempoActivo: { type: Number, default: 0 },
+  amigos: { type: Number, default: 0 },
+  logrosObtenidos: { type: Number, default: 0 },
+  easterEggs: { type: Number, default: 0 },
+  juegosCompletadas: { type: Number, default: 0 },
+  completado: { type: Boolean, default: false },
+  misjuegos: { type: Boolean, default: false },
+  wishlist: { type: Boolean, default: false },
+})
+
+datauserSchema.index({ usuarioId: 1, juegoId: 1 }, { unique: true })
+
+export default mongoose.model('Datauser', datauserSchema)
